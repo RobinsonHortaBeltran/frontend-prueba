e20831b6a7d388672e835fb16725427bcdef8952
Robinson Horta <hortarobinson@gmail.com>
2023-04-18 23:28:23 -0500

    feat(initialize project)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { NewComponent } from './views/new/new.component';
+import { EditComponent } from './views/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    NewComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule
